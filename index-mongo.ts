09bc35eee12a85c8298c5a9e41c84f54e3a9d77d
Juliabritto55 <julia.britto@estudante.ifms.edu.br>
2025-09-17 12:01:36 -0300

    recontruindo index-mongo.ts

--- index-mongo.ts
+++ index-mongo.ts
@@ -1,24 +1,21 @@
-import express from 'express'
-import type { Request, Response } from 'express'
+import express, { Request, Response } from 'express'
 import 'dotenv/config'
 import { MongoClient } from 'mongodb'
 
 const client = new MongoClient(process.env.MONGO_URI!)
 await client.connect()
-const db = client.db(process.env.MONGO_DB)
+const db = client.db(process.env.MONGO_DB!)
 
 const app = express()
-
-// Middleware para fazer o parse do body da requisição para JSON
+//Esse middleware faz com que o 
+// express faça o parse do body da requisição para json 
 app.use(express.json())
-
-// Rota para listar produtos
-app.get('/produtos', async (req: Request, res: Response) => {
+//criando uma rota para acesso pelo navegador
+app.get('/produtos', async (req:Request, res:Response) => {
     const produtos = await db.collection('produtos').find().toArray()
     res.json(produtos)
 })
-
-// Inicializa o servidor na porta 8000
+// Criando o servidor na porta 8000 com o express
 app.listen(8000, () => {
     console.log('Server is running on port 8000')
 })
