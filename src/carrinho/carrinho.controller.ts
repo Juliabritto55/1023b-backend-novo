3ab78b8223e11448f2754370c5b0fbf4d13f0602
Juliabritto55 <julia.britto@estudante.ifms.edu.br>
2025-10-06 08:39:20 -0300

    arrumando codigo

--- src/carrinho/carrinho.controller.ts
+++ src/carrinho/carrinho.controller.ts
@@ -1,11 +1,57 @@
-import { Request, Response } from 'express'
-class CarrinhoController{
-    adicionar(req:Request, res:Response) {
-        res.send("Terezinho de Jesus")
-    }
-    listar(req:Request, res:Response) {
-        res.send("Terezinho de Jesus")
-    }
+import { Request, Response } from "express";
+
+interface ItemCarrinho {
+    produtoId: string;
+    quantidade: number;
+    precoUnitario: number;
+    nome: string;
 }
 
-export default new CarrinhoController()
+interface Carrinho {
+    usuarioId: string;
+    itens: ItemCarrinho[];
+    dataAtualizacao: Date;
+    total: number;
+}
+class CarrinhoController {
+    //adicionarItem
+    async adicionarItem(req:Request, res:Response) {
+        const { usuarioId, produtoId, quantidade } = req.body;
+    } 
+        //Buscar o produto no banco de dados
+        const produto = await produtosController.buscarPorId(produtoId);
+        if (!produto) {
+            return res.status(404).json({ mensagem: "Produto não encontrado" });
+        }
+        const precoUnitario = produto.preco;
+        const nome = produto.nome;
+        //Pegar o preço do produto
+        //Pegar o nome do produto
+
+
+        // Verificar se um carrinho com o usuário já existe
+        let carrinho = await buscarCarrinhoPorUsuarioId(usuarioId);
+
+        const item: ItemCarrinho = {
+            produtoId,
+            quantidade,
+            precoUnitario
+
+        // Se não existir deve criar um novo carrinho
+
+        // Se existir, deve adicionar o item ao carrinho existente
+
+        // Calcular o total do carrinho
+
+        // Atualizar a data de atualização do carrinho
+
+
+
+
+    //removerItem
+    //atualizarQuantidade
+    //listar
+    //remover                -> Remover o carrinho todo
+
+}
+export default new CarrinhoController();
